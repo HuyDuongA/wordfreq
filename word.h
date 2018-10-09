b23d9c5f929291043885797d0954ce63fc996de7
Huy Duong <haduong@calpoly.edu>
2018-10-08 20:16:21 -0700

    Adding count functions

--- word.h
+++ word.h
@@ -8,17 +8,19 @@ typedef struct node{
    struct node *next;
 }word;
 
-unsigned int hash_function(char *);       //done
-word *new_word(char *);                   //Huy
-void evaluate_word(char *, word **);      //Huy
-void append_word(char *, word **);        //Visal
-word *look_up_word(char *, word **);      //Visal
-int count(word **);                       //Huy
-word *hash_to_list(word **);              //Huy
-int wordcmp(word* , word*);               //Visal & Huy
-void sort_print(int ,word *, word **);    //Huy
-void clean_up(word *, word **);           //Huy
-char *read_word(FILE *);                  //done
-int read_file_check(const char*);         //done
-void process_word_to_hashtable(FILE *, word **);
+unsigned int hash_function(char *);                 //done
+word *new_word(char *);                             //Huy
+void evaluate_word(char *, word **);                //Huy
+void append_word(char *, word **);                  //Visal
+word *look_up_word(char *, word **);                //Visal
+int count_hash(word **);                            //Huy
+int count_list(word *);                             //Huy
+word *hash_to_list(word **);                        //Huy
+int wordcmp(word* , word*);                         //Visal & Huy
+void sort_print(int ,word *);                       //Huy
+void clean_up_list(word *);                         //Huy
+void clean_up_hash(word **);                        //Huy
+char *read_word(FILE *);                            //done
+int read_file_check(const char*);                   //done
+void process_word_to_hashtable(FILE *, word **);    //Visal
 #endif
